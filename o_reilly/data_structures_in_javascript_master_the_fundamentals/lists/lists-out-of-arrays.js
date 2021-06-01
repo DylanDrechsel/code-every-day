5b99bdb0c019e3ab28d012411693d419497ac28d
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-01 12:49:56 -0400

    Started chapter 3: Lists & completed first lesson

---
+++ o_reilly/data_structures_in_javascript_master_the_fundamentals/lists/lists-out-of-arrays.js
@@ -0,0 +1,8 @@
+// Lists
+// A data structure built out of an array
+// Appears to have a 'squishy' size where elements can be added or removed
+// Supports operations like:
+let x = ll.get(index)
+ll.set(index, value)
+ll.removeFromIndex(index)
+ll.insertAtIndex(index, value)
