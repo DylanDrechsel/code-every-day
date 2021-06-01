7a690e63388cb1de27534f68cdde3cb64199d613
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-01 15:56:57 -0400

    Completed implementing list remove lesson

---
+++ o_reilly/data_structures_in_javascript_master_the_fundamentals/lists/implementing-list-remove.js
@@ -0,0 +1,82 @@
+class List {
+	constructor() {
+		this.data = new Array(10);
+		this.size = 0;
+	}
+
+	// add the value to the first-most free position in the data array
+	push(value) {
+		if (this.size === this.data.length) {
+			this.grow();
+		}
+
+		this.data[this.size] = value;
+		this.size++;
+	}
+
+    // removed(index = 1)
+    // array = [5, 8, 12, 13, 19]
+    // removed = 8
+    remove(index) {
+        if (this.size === 0) {
+            return null
+        }
+
+        // save the value that was at the index
+        let removed = this.data[index]
+        // scoot all values after over left by 1
+        for (let i = index; i < this.size - 1; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        // manually overwrite the now-stale data
+        this.data[this.size - 1] = null
+        // decrement the size to show one item was removed
+        this.size--
+        // return the value of the removed item
+        return removed
+    }
+
+	grow() {
+		// create a new array thats twice as big
+		let array = new Array(this.data.length * 2);
+
+		// copy over every value from the old data to the larger array
+		for (let i = 0; i < this.data.length; i++) {
+			array[i] = this.data[i];
+		}
+		// set the larger area as the data for the list
+		this.data = array;
+	}
+
+	toString() {
+		if (this.size === 0) {
+			return '[]';
+		} else {
+			let result = '';
+			for (let i = 0; i < this.size; i++) {
+				result += this.data[i] + ' ';
+			}
+			return `[ ${result} ]`;
+		}
+	}
+}
+
+const ll = new List();
+ll.push(5)
+ll.push(8);
+ll.push(12);
+ll.push(13);
+ll.push(19);
+
+console.log(ll.toString());
+console.log(ll.data)
+
+ll.remove(1)
+
+console.log(ll.toString());
+console.log(ll.data);
+
+ll.push(82)
+
+console.log(ll.toString());
+console.log(ll.data);
