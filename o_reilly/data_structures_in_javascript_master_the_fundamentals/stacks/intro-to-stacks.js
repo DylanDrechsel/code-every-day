e7fc5df8fac95a4521b478b3ecb3fd203ab3bad5
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-09 10:53:38 -0400

    Completed intro to stacks lesson

---
+++ o_reilly/data_structures_in_javascript_master_the_fundamentals/stacks/intro-to-stacks.js
@@ -0,0 +1,14 @@
+// Stacks
+// Stacks only have two operations
+// .push() & .pop()
+
+// Stack is a class with 4 methods
+// .push - adds to top 0(1)
+// .pop - removes from top 0(1)
+// .peek - looks at top 0(1)
+// .size() 0(1)
+// Stacks have no random access
+
+// is good with undo/redo operations
+// parenthesis matching
+// function calls
