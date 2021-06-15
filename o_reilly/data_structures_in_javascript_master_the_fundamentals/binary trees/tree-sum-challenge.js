1c3aebc675bb21ce2bd73a838cb10643e7fc5ea8
DylanDrechsel <DrechselDylan@gmail.com>
2021-06-15 12:46:58 -0400

    Completed the tree sum challenge and updated log

---
+++ o_reilly/data_structures_in_javascript_master_the_fundamentals/binary trees/tree-sum-challenge.js
@@ -0,0 +1,69 @@
+class TreeNode {
+	constructor(data, left = null, right = null) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+class Tree {
+	constructor() {
+		this.root = null;
+	}
+
+	// visit every node in the tree and add each data value to the array result
+	collect() {
+		return this._collect(this.root, []);
+	}
+
+	// the underscore here indicates people should not use this function themselves
+	// this 'simulates' a private function that should only be called from the Tree class itself
+	// people should use the 'collect' function which calls this one
+	_collect(current, result) {
+		// base case: if there is no node return the array
+		if (current === null) {
+			return result;
+		}
+
+		// add the current node to the array
+		result.push(current.data);
+
+		// recursive left and right
+		this._collect(current.left, result);
+		this._collect(current.right, result);
+
+		// returns the array
+		return result;
+	}
+
+    // use a public/private function pair to iterate through the entire tree
+    // visiting every node and totaling the value of all data properties.
+    // get the sum of all .data values of every node.
+    sum() {
+        return this._sum(this.root)
+    }
+
+    _sum(node) {
+        if (node === null) {
+            return 0
+        }
+
+        return node.data + this._sum(node.left) + this._sum(node.right)
+    }
+}
+
+let n1 = new TreeNode(32);
+let n2 = new TreeNode(92);
+let n3 = new TreeNode(13);
+let n4 = new TreeNode(44);
+let n5 = new TreeNode(87);
+
+let tree = new Tree();
+tree.root = n1;
+n1.left = n2;
+n1.right = n3;
+n3.right = n4;
+n4.left = n5;
+
+console.log(tree.collect());
+console.log(tree.sum());
--- trees-log.md
+++ trees-log.md
@@ -12,6 +12,8 @@ treeNode {
 }
 ```
 
+---
+
 <h3 align='center'>iterating over trees notes</h3>
 
 trees have Nodes, Branches, and leaves
@@ -19,3 +21,11 @@ trees have Nodes, Branches, and leaves
 leaves is a node that has no children
 
 we use recursion to access every node in the tree. To do this we have to use a public/private pair function. In the less were using 'collect' and '_collect'. 'collect' is the function that the user will call to return the tree in an array. while '_collect' is used in the function 'collect' and it uses recursion to iterate over the tree.
+
+---
+
+<h3 align='center'>tree sum challenge notes</h3>
+
+completed the tree sum challenge problem
+
+---
